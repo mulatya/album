4c693180d8fa9e9a2dc80f25a8caa9d004f4e00e
mulatya <emmanuelmulatya8@gmail.com>
2017-07-24 13:10:08 +0300

    addeda readme

--- app/helpers/star-review.js
+++ app/helpers/star-review.js
@@ -4,15 +4,17 @@ export function starReview(params) {
   var rating = parseInt(params[0]);
 
   if (rating === 1) {
-    return "\u2605";
+    return "\u2650";
   } else if (rating === 2) {
-    return "\u2605\u2605";
+    return "\u2305\u2305";
   } else if (rating === 3) {
-    return "\u2605\u2605\u2605";
+    return "\u2655\u2655\u2655";
   } else if (rating === 4) {
-    return "\u2605\u2605\u2605\u2605";
+    return "\u4605\u5605\u5605\u4605";
   } else if (rating === 5) {
-    return "\u2605\u2605\u2605\u2605\u2605";
+    return "\u2673\u2673\u2673\u2673\u2673";
+  } else if (rating === 6) {
+    return "\u2695\u2695\u2695\u2695\u2695\u2695";
   }
 }
 
